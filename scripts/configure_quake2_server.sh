d1e693ac866e9837c65712edd6f63f6fef90c6f0
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2019-06-23 17:46:50 +0200

    Updated specific breakout for CTF

--- scripts/configure_quake2_server.sh
+++ scripts/configure_quake2_server.sh
@@ -9,13 +9,23 @@ SERVERDIR="/usr/share/games/quake2"
 # Set up all symlinks to config files
 link_configs()
 {
-  # Check all config files
+  # Check all config files for baseq2
   for i in $(ls ${CONFIGDIR}/*.cfg); do
     # If the file is not symlinked then do so
     if ! [[ -f ${SERVERDIR}/baseq2/$(basename $i) ]]; then
       ln -s $i ${SERVERDIR}/baseq2/$(basename $i)
     fi 
   done
+
+  # Check all config files for ctf
+  if [[ ${SERVERDIR}/ctf ]]; then
+    if [[ -f ${SERVERDIR}/ctf/server.cfg ]]; then
+      rm ${SERVERDIR}/ctf/server.cfg
+    fi
+
+    # Symlink for CTF
+    ln -s ${CONFIGDIR}/ctf.cfg ${SERVERDIR}/ctf/server.cfg
+  fi
 }
 
 link_configs
